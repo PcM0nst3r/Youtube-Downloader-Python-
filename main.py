af45319245a816d102b7f22ee7fea21b62b37d44
unknown <damedamedame7@gmail.com>
2022-05-10 17:58:13 +0200

    'First'

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import tkinter.messagebox
+import tkinter as tk
+from pytube import YouTube
+
+yt = tk.Tk()
+yt.title('YouTube Downloader')
+yt.resizable(None, None)
+yt.geometry('400x150')
+
+entry1 = tk.Entry(yt)
+entry1.pack(pady=10)
+
+
+def getAudioDownload():
+    x1 = entry1.get()
+
+    def complete(n1, n2):
+        tkinter.messagebox.showinfo(title='Done', message=n2[:-1]+'3')
+        base = os.path.splitext(n2)[-2]
+        print(base)
+        os.rename(n2, base + '.mp3')
+        print(n2)
+
+    video_object = YouTube(x1, on_complete_callback=complete)
+
+    label1 = tk.Label(yt, text=video_object.title)
+    label1.pack(pady=10)
+
+    video_object.streams.get_audio_only().download()
+
+
+def getHVideoDownload():
+
+    x1 = entry1.get()
+
+    def complete(n1, n2):
+        tkinter.messagebox.showinfo(title='Done', message=n2)
+
+    video_object = YouTube(x1,on_complete_callback=complete)
+
+    label1 = tk.Label(yt, text=video_object.title)
+    label1.pack(pady=10)
+
+    video_object.streams.get_highest_resolution().download()
+
+
+b1=tk.Button(text='Get Audio', command=getAudioDownload)
+b1.pack()
+
+b2 = tk.Button(text='Get Video', command=getHVideoDownload)
+b2.pack()
+
+yt.mainloop()
+
